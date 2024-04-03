4bbb908d75aec927b11cd5248d51b1b3b17d0cae
SaurabhExpertInnovator <165933863+SaurabhExpertInnovator@users.noreply.github.com>
2024-04-03 22:26:20 +0530

    sum.c
    This file consist C program to add two numbers

---
+++ sum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+
+//Taking two numbers as input from user
+printf("Enter first number : );
+scanf("%d",&a);
+printf("Enter second number : );
+scanf("%d",&b);
+
+//Calculating sum of numbers
+int sum=a+b;
+
+//printing the result
+printf("The sum of two numbers is : %d");
+return 0;
+}
